b22ab5b0d042c1ca1ca0eaedc35bfe766a545351
Jacques-Etienne Baudoux <je@bcim.be>
2015-06-15 19:14:41 +0200

    [FIX] account_companyweb: flake8, README.rst

--- account_companyweb/__openerp__.py
+++ account_companyweb/__openerp__.py
@@ -4,6 +4,7 @@
 #
 #    Authors: Adrien Peiffer
 #    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    Copyright (c) 2015 BCIM sprl (http://www.bcim.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -23,7 +24,7 @@
 {
     "name": "Companyweb",
     "version": "1.0",
-    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,BCIM sprl,Odoo Community Association (OCA)",
     "category": "Generic Modules/Accounting",
     "website": "http://www.acsone.eu",
     "depends": [
@@ -38,44 +39,11 @@
     'external_dependencies': {
         'python': ['lxml', 'xlwt', 'xlrd'],
     },
-    "description": """
-Companyweb - Know who you are dealing with
-==========================================
-
-This module provides access to financial health information about Belgian
-companies right from the OpenERP Customer form. Information is obtained
-from the Companyweb database (www.companyweb.be).
-
-You must be a Companyweb customer to use this module in production.
-Please visit www.companyweb.be and use login 'cwacsone',
-with password 'demo' to obtain test credentials.
-
-Main Features
--------------
-* Obtain crucial information about Belgian companies,
-  based on their VAT number: name, address,
-  credit limit, health barometer, financial informations
-  such as turnover or equity capital, and more.
-* Update address and credit limit in your OpenERP database.
-* Generate reports about payment habits of your customers.
-* Access to detailed company information on www.companyweb.be.
-
-Technical information
----------------------
-This module depends on module account_financial_report_webkit which
-provides an accurate algorithm for open invoices report.
-
-Contributors
-------------
-* Stéphane Bidoul <stephane.bidoul@acsone.eu>
-* Adrien Peiffer <adrien.peiffer@acsone.eu>
-""",
     "data": [
         "view/res_config_view.xml",
         "view/res_partner_view.xml",
         "wizard/partner_update_companyweb.xml",
     ],
-    "demo": [],
     "license": "AGPL-3",
     "installable": True,
 }
